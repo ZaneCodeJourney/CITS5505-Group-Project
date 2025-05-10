881878758999e2930dbd8feddbae27e8ee7ee414
Ziyang Wen <wenziyang007@gmail.com>
2025-05-10 11:11:43 +0800

    DL-TEST-003: Unit tests for share functions

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -8,7 +8,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../.
 if __name__ == '__main__':
     # Discover and run all tests in the tests/auth directory
     test_loader = unittest.TestLoader()
-    test_suite = test_loader.discover('tests/auth', pattern='test_*.py')
+    test_suite = test_loader.discover('tests', pattern='test_*.py')
     
     # Run the tests
     result = unittest.TextTestRunner(verbosity=2).run(test_suite)
