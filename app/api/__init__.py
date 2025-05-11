59699f1ffa0a6d0ccba8255dc0d6acb584228755
Ziyang Wen <wenziyang007@gmail.com>
2025-05-11 14:32:09 +0800

    DL-FE-004: Fix bug

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -5,4 +5,4 @@ from flask import Blueprint
 bp = Blueprint('api', __name__, url_prefix='/api')
 
 # Import routes at the bottom to avoid circular imports
-from app.api import routes 
+from app.api import routes, auth 
